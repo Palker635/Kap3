719373a8a2fd630b0abeb8ab70c939a8153101ef
Isac Hansson <isac.hansson@learnet.se>
2019-09-26 11:16:33 +0200

    torsdag

---
+++ Uppgift32.java
@@ -0,0 +1,18 @@
+/*
+Uppgift 3.2
+Skapat 2019-09-26
+Dev: Isac Hansson
+*/
+
+
+public class Uppgift32 {
+	public static void main (String[] args) {
+	byte a; 
+	String b; 
+	int c; 
+	// funkar ej eftersom "int" inte kan innehålla decimaltal - det behövs en "double". 
+	a = 500,7;
+	b = 500,7; 
+	c = 500,7; 
+	
+}
---
+++ Uppgift33.java
@@ -0,0 +1,11 @@
+/*
+Uppgift 3.1
+Skapat 2019-09-26
+Dev: Isac Hansson
+*/
+
+
+public class Uppgift33{
+	public static void main (String[] args) {
+	
+}
