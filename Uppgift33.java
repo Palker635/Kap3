ca90134e8ffbc0a9545d738dac10e078e8b801ac
Isac Hansson <isac.hansson@learnet.se>
2019-09-27 11:18:52 +0200

    fredag

--- Uppgift33.java
+++ Uppgift33.java
@@ -4,8 +4,13 @@ Skapat 2019-09-26
 Dev: Isac Hansson
 */
 
-
-public class Uppgift33{
+import javax.swing.*; 
+public class Uppgift33	{
 	public static void main (String[] args) {
+	double pi = 3.1415; 
+	double radie;
 	
+	radie = Double.parseDouble (JOptionPane.showInputDialog(null, "Vilken radie har din cirkel?"));
+	JOptionPane.showMessageDialog(null, radie*radie*pi);
+		}
 }
